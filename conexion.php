092a91d0d1c06b596f6ba00c88b84791d6ea355b
Bruno Caruso <brunofcaruso@gmail.com>
2018-11-06 11:04:13 -0300

    Añadida conexion a MySQL

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "127.0.0.1";
-$usuario = "root";
-$pass = "";
-$db = "feedlot";
+$host = "us-cdbr-iron-east-01.cleardb.net";
+$usuario = "b33d3bf976f347";
+$pass = "38b9929e";
+$db = "feed-lot";
 
 $con = mysqli_connect ($host, $usuario, $pass, $db);
 
